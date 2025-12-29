f643b0d7432a40cbafc9253c14e1a2e4b3e7822c
archie1345 <saskaraarchie@gmail.com>
2025-12-29 17:05:53 +0700

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { CopyProvider } from './widgets/copyContext';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Homepage from './App.jsx'
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <CopyProvider>
-    <Homepage />
+    <App />
   </CopyProvider>,
 )
